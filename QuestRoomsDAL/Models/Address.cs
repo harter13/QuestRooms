acf1cdd7640d6c940a2115fc1ff9a286ea4cd7bb
harter_13 <harter_13@ukr.net>
2019-08-19 12:00:54 +0300

    2

---
+++ QuestRoomsDAL/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestRoomsDAL.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public virtual Country Country { get; set; }
+        public virtual City City { get; set; }
+        public virtual Street Street { get; set; }
+        public string HouseNumber { get; set; }
+        public virtual ICollection<Room> Rooms { get; set; }
+    }
+}
---
+++ QuestRoomsDAL/Models/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestRoomsDAL.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TimeSpan PassingTime { get; set; }
+        public int NumberPlayers { get; set; }
+        public int YearPlayers { get; set; }
+        public virtual Address Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public virtual Company Company { get; set; }
+        public int Rating { get; set; }
+        public int LevelFear { get; set; }
+        public int LevelDifficulty { get; set; }
+        public string Logo { get; set; }
+        public string Images { get; set; }
+
+    }
+}
